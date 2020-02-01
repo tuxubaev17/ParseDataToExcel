2959b24949132eec7e11d9ba8a7fd0f7667cbaa7
tuxubaev17 <tuxubaev17@mail.com>
2020-02-01 18:12:04 +0600

    write data to excel

--- choco_page.py
+++ choco_page.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 import time
 
+import pandas as pd
+import xlsxwriter
+
 link = 'https://chocofood.kz'
 chromedriver = '/Users/alikhantuxubayev/Desktop/task/chromedriver'
-
 
 browser = webdriver.Chrome(chromedriver)
 browser.get(link)
@@ -11,8 +13,19 @@ browser.implicitly_wait(5)
 address = 'улица Байзакова 280'
 address_field = browser.find_element_by_class_name('address-field__input').send_keys(address)
 button = browser.find_element_by_class_name('submit-button').click()
-browser.implicitly_wait(5)
-info = browser.find_element_by_css_selector(".rl-list__items .rl-list__items__item__name").text
-print(info)
 time.sleep(5)
+name = []
+s = []
+s = browser.find_elements_by_css_selector(".rl-list__items__item__name")
+for elm in s:
+    name.append(elm.text)
+
+time.sleep(15)
 browser.quit()
+
+df = pd.DataFrame({'Data':name})
+writer = pd.ExcelWriter('name.xlsx', engine='xlsxwriter')
+df.to_excel(writer, sheet_name='Sheet1')
+writer.save()
+
+
