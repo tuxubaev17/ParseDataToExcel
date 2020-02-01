0f6d7d58987b84605f6424bca9a214a15aa05a43
tuxubaev17 <tuxubaev17@mail.com>
2020-02-01 18:51:06 +0600

    update

--- choco_page.py
+++ choco_page.py
@@ -5,7 +5,7 @@ import pandas as pd
 import xlsxwriter
 
 link = 'https://chocofood.kz'
-chromedriver = '/Users/alikhantuxubayev/Desktop/task/chromedriver'
+chromedriver = '/Users/alikhantuxubayev/Documents/ChocoParser/chromedriver'
 
 browser = webdriver.Chrome(chromedriver)
 browser.get(link)
@@ -13,19 +13,19 @@ browser.implicitly_wait(5)
 address = 'улица Байзакова 280'
 address_field = browser.find_element_by_class_name('address-field__input').send_keys(address)
 button = browser.find_element_by_class_name('submit-button').click()
-time.sleep(5)
-name = []
-s = []
-s = browser.find_elements_by_css_selector(".rl-list__items__item__name")
-for elm in s:
-    name.append(elm.text)
+browser.implicitly_wait(10)
+
+data = []
+names = []
+names = browser.find_elements_by_css_selector(".rl-list__items__item__name")
+for elm in names:
+    data.append(elm.text)
+
 
 time.sleep(15)
 browser.quit()
 
-df = pd.DataFrame({'Data':name})
+df = pd.DataFrame({'name': data})
 writer = pd.ExcelWriter('name.xlsx', engine='xlsxwriter')
 df.to_excel(writer, sheet_name='Sheet1')
 writer.save()
-
-
