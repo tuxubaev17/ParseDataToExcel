85ec7b2c6d60e424ad5153e112ed724b66109de3
tuxubaev17 <tuxubaev17@mail.com>
2020-02-01 19:50:58 +0600

    mod

--- choco_page.py
+++ choco_page.py
@@ -1,31 +1,41 @@
 from selenium import webdriver
 import time
-
 import pandas as pd
 import xlsxwriter
 
 link = 'https://chocofood.kz'
 chromedriver = '/Users/alikhantuxubayev/Documents/ChocoParser/chromedriver'
-
 browser = webdriver.Chrome(chromedriver)
-browser.get(link)
-browser.implicitly_wait(5)
-address = 'улица Байзакова 280'
-address_field = browser.find_element_by_class_name('address-field__input').send_keys(address)
-button = browser.find_element_by_class_name('submit-button').click()
-browser.implicitly_wait(10)
+
+
+def write_data():
+    df = pd.DataFrame({'name': data})
+    writer = pd.ExcelWriter('name.xlsx', engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1')
+    writer.save()
+
+
+def open(url):
+    browser.get(url)
+    browser.implicitly_wait(5)
+
+
+def steps(data):
+    open(link)
+    address = 'улица Байзакова 280'
+    address_field = browser.find_element_by_class_name('address-field__input').send_keys(address)
+    button = browser.find_element_by_class_name('submit-button').click()
+    browser.implicitly_wait(15)
+
+    names = browser.find_elements_by_css_selector(".rl-list__items__item__name")
+    for elm in names:
+        data.append(elm.text)
+        print(elm.text)
+
+    time.sleep(15)
+    browser.quit()
+    write_data()
+
 
 data = []
-names = []
-names = browser.find_elements_by_css_selector(".rl-list__items__item__name")
-for elm in names:
-    data.append(elm.text)
-
-
-time.sleep(15)
-browser.quit()
-
-df = pd.DataFrame({'name': data})
-writer = pd.ExcelWriter('name.xlsx', engine='xlsxwriter')
-df.to_excel(writer, sheet_name='Sheet1')
-writer.save()
+steps(data)
